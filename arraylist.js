7775812f890c1e0ff331288bfc9a3088709ebe3b
APPS <UserAdmin2@johns-air.home>
2022-12-12 14:37:01 +0000

    firstcommit

---
+++ arraylist.js
@@ -0,0 +1,46 @@
+let todaysDate = new Date ()
+console.log (`Hello Boss, the time is ${todaysDate}`)
+//      --MY CUSTOMISED WELCOME MESSAGE--
+
+
+
+let favHolidayDestinations = [
+    "Venice, Italy",
+    "Paris, France",
+    "Barcelona, Spain",
+
+]
+
+console.log (favHolidayDestinations[2]= "Florida, USA")
+// method to create a list - FAVOURITE HOLIDAY DESTINATIONS.
+// Adding brackets with number inside after console.log code selects a specific item of the list.
+// Adding "Florida, USA" at the end of console.log willl replace the destination in the selected list.
+
+
+let favbooks = [
+    "Americanah",
+    "Snowball",
+    "Rich dad Poor dad",
+
+]
+
+console.log (favbooks.pop())
+// Method to create a list - FAVOURITE BOOKS.
+// Adding brackets with number inside after console.log code selects a specific item of the list.
+// adding .length calculate the total items in the list
+// Adding (.pop()) adds the last item on the list and prints it out on the terminal
+
+
+let favMovies = [
+    "Titanic",
+    "Grinch",
+    "Home Alone",
+
+]
+
+console.log(favMovies.shift())
+// Method to create a list - FAVOURITE MOVIES.
+// Adding (.shift)) adds the first item on the list and prints it out on the terminal
+
+
+
