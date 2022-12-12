6353397db8116d1d08db01cb9efc85ef550c5a2b
APPS <UserAdmin2@johns-air.home>
2022-12-12 14:21:01 +0000

    firstcommit

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log("Hello World")
+// This is a string ("Helllo World") - FOR REPRESENTING TEXT
+// console.log is a METHOD
+
+console.log("Hello".length)
+// (.length) represents the numbers - LENGTH OF STRING IN MUMERICAL DISPLAY (NUMBERS)
+
+console.log("Hello, what is your name?".length)
+// (.length) represents the numbers - LENGTH OF STRING IN MUMERICAL DISPLAY (NUMBERS)
+
+console.log("Hello".toUpperCase)
+// This is the WRONG METHOD of writing a sting in UPPERCASE - NEED TO ADD ADDITIONAL OPEN/CLOSING BRACKET TO ORIGINAL BRACKETS
+
+console.log("Hello".toUpperCase())
+// This is the RIGHT METHOD of writing a sting in UPPERCASE- PARAMETERS
+
+// This is known as a a comment I can write anything here without it saving on my code - PRESS THE COMMAND + FORWARD SLASH BUTTONS.
+// Strings have METHODS that we can use to manipulate them.
+// Dot notation (.) is used to access the property of an object.
+
+console.log("Hello".toLowerCase())
+// This is the RIGHT METHOD of writing a sting in LOWERCASE- PARAMETERS
+
+const greetings = "   Hello Johnny   "
+console.log(greetings)
+console.log(greetings.trim())
+// The trim() method removes whitespace from both ends of a string and returns a new string, without modifying the original string.
+
+
